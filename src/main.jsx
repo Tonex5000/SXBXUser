a7ecd1e6687121f01024f345450be44ca4a35782
Antex <tonypetalala@gmail.com>
2025-04-05 20:47:18 -0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './Admin.jsx'
+import App from './User.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
